fcea17abe82605d9481c8d00d38d44fdad20b816
samochkov.aa <project_corba@list.ru>
2024-03-05 16:19:44 +0300

    Неделя 2 3 видео
    Вид от первого лица

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -4,23 +4,36 @@ using UnityEngine;
 public class Controller : MonoBehaviour
 {
     [SerializeField] PlayerCharacter _player;
+    [SerializeField] private float _mouseSensetivity = 2f;
+    [SerializeField] private int _mouseInversion = -1;
     void Update()
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
 
-        _player.SetInput(h, v);
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        bool space = Input.GetKeyDown(KeyCode.Space);
+
+        _player.SetInput(h, v, mouseX * _mouseSensetivity);
+        _player.RotateX(_mouseInversion * mouseY * _mouseSensetivity);
+        if (space) _player.Jump();
 
         SendMove();
     }
 
     private void SendMove()
     {
-        _player.GetMoveInfo(out Vector3 position);
+        _player.GetMoveInfo(out Vector3 position, out Vector3 velocity);
 
         Dictionary<string, object> data = new Dictionary<string, object>(){
-            {"x", position.x},
-            {"y", position.z}
+            {"pX", position.x},
+            {"pY", position.y},
+            {"pZ", position.z},
+            {"vX", velocity.x},
+            {"vY", velocity.y},
+            {"vZ", velocity.z}
         };
         MultiplayerManager.Instance.SendMessage("move", data);
     }
--- Assets/Scripts/EnemyCharacter.cs
+++ Assets/Scripts/EnemyCharacter.cs
@@ -2,8 +2,28 @@ using UnityEngine;
 
 public class EnemyCharacter : MonoBehaviour
 {
-    public void SetPosistion(Vector3 position)
+    public Vector3 targetPosition { get; private set; } = Vector3.zero;
+    private float _velocityMagnitude = 0f;
+
+    private void Start()
     {
-        transform.position = position;
+        targetPosition = transform.position;
+    }
+    private void Update()
+    {
+        if(_velocityMagnitude > .1f)
+        {
+            float maxDistance = _velocityMagnitude * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, maxDistance);
+        }
+        else
+        {
+            transform.position = targetPosition;
+        }
+    }
+    public void SetMovement(in Vector3 position, in Vector3 velocity, in float averageInterval)
+    {
+        targetPosition = position + (velocity * averageInterval);
+        _velocityMagnitude = velocity.magnitude;
     }
 }
